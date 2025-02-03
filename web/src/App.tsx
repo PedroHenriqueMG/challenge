0fdb7e4a7614e8f857f4a2c3cf4ec112dd47f99f
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2025-02-03 09:55:53 -0300

    style: style app front layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,9 @@ import { TableTasks } from "./components/tableTasks/indes";
 export function App() {
   return (
     <QueryClientProvider client={new QueryClient()}>
-      <TableTasks />
+      <section className="min-h-screen flex flex-col justify-center items-center">
+        <TableTasks />
+      </section>
     </QueryClientProvider>
   );
 }
